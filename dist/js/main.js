7591d9268b2bb8fa9e3cd97cb65e82f1c8db516d
CườngTrần <interstar0406@gmail.com>
2018-09-20 16:03:25 +0700

    fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,80 +31,21 @@ $(function () {
 		$('select.form-control').removeClass('form-control').css('width', '100%');
 	}
 });
-
-//khai báo object
-
-var cuongApp = {
-	ten: 'CƯờng',
-	tuoi: 21,
-	dienthoai: ['iphone', 'huawei', 'vivo'],
-	xe: [
-	//các obj con phải có key giống nhau
-	{
-		time: 1,
-		model: 'heo'
-	}, {
-		time: 2,
-		model: 'trâu'
-	}]
-};
+// Main
+console.log('FullStack Vietnam!');
 $(document).ready(function () {
-	for (var i = 0; i < cuongApp.dienthoai.length; i++) {
-		console.log(cuongApp.dienthoai[i]);
-	}
-	for (var _i = 0; _i < cuongApp.xe.length; _i++) {
-		console.log(cuongApp.xe[_i].model);
-	}
-});
-
-$(document).ready(function () {
-
-	// //ẩn hiện các tag
-	// $('.home_nay .item .body').hide()
-	// $('.home_nay .item .title-do').on('click', function(){
-	// 	console.log('clicked')
-	// 	$('.home_nay .item .body').hide()
-	// 	$(this).parents('.item').find('.body').show()
-
-	// })
-
-	//thay đổi item theo time
-	// setTimeout(function () {
-	// 	$('body').css({
-	// 		"background":"blue"
-	// 	})
-	// },3000)
-
-	// setInterval(function(){
-	// 	$('.body').append('àmkạbdfkahsbkfhaiebfc kạnlfnaẹa hlại l ja')
-	// }, 1000)
-
-
-	//vòng lặp
-	$('h2').each(function () {
-		console.log($(this).text());
-	});
-	// css inline thẻ title
-	$(".title").css({
-		"background": "red"
-	});
-	$('.home_clients .owl-carousel').owlCarousel({
+	$('.owl-carousel').owlCarousel({
 		loop: true,
-		items: 1,
+		margin: 10,
 		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left owl-left"></i>', '<i class="fa fa-chevron-right owl-right"></i>'],
 		responsive: {
-			// breakpoint from 480 up
-			480: {
-				items: 2
+			0: {
+				items: 1
 			},
-			// breakpoint from 768 up
-			768: {
-				items: 4
+			600: {
+				items: 3
 			},
-			// breakpoint from 992 up
-			992: {
+			1000: {
 				items: 5
 			}
 		}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,92 +1,20 @@
-
-
-//khai báo object
-
-var cuongApp ={
-	ten: 'CƯờng',
-	tuoi: 21,
-	dienthoai: [
-		'iphone',
-		'huawei',
-		'vivo'
-	],
-	xe: [
-//các obj con phải có key giống nhau
-		{
-			time: 1,
-			model: 'heo'
-		},
-		{
-			time: 2,
-			model: 'trâu'
-		}
-	]
-}
-$(document).ready(function(){
-	for(let i=0; i<cuongApp.dienthoai.length;i++){
-		console.log(cuongApp.dienthoai[i])
-	}
-	for(let i=0; i<cuongApp.xe.length;i++){
-		console.log(cuongApp.xe[i].model)
-	}
-})
-
-
-
-
+// Main
+console.log('FullStack Vietnam!')
 $(document).ready(function () {
-
-
-	// //ẩn hiện các tag
-	// $('.home_nay .item .body').hide()
-	// $('.home_nay .item .title-do').on('click', function(){
-	// 	console.log('clicked')
-	// 	$('.home_nay .item .body').hide()
-	// 	$(this).parents('.item').find('.body').show()
-	
-	// })
-
-	//thay đổi item theo time
-	// setTimeout(function () {
-	// 	$('body').css({
-	// 		"background":"blue"
-	// 	})
-	// },3000)
-	
-	// setInterval(function(){
-	// 	$('.body').append('àmkạbdfkahsbkfhaiebfc kạnlfnaẹa hlại l ja')
-	// }, 1000)
-	
-	
-	
-	
-	//vòng lặp
-	$('h2').each(function(){
-		console.log($(this).text())
-	})
-	// css inline thẻ title
-	$(".title").css({
-		"background":"red"
-	})
-	$('.home_clients .owl-carousel').owlCarousel({
-		loop: true,
-		items: 1,
-		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left owl-left"></i>', '<i class="fa fa-chevron-right owl-right"></i>'],
-		responsive: {
-			// breakpoint from 480 up
-			480: {
-				items: 2
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		responsive:{
+			0:{
+				items:1
 			},
-			// breakpoint from 768 up
-			768: {
-				items: 4
+			600:{
+				items:3
 			},
-			// breakpoint from 992 up
-			992: {
-				items: 5
+			1000:{
+				items:5
 			}
 		}
-	});
-});
+	})
+})
